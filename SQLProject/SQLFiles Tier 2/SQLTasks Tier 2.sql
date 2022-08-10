9f4e012aa8df5e079f2345ad97788504768728bb
nahsradus <nahsradus@gmail.com>
2022-08-09 22:22:36 -0700

    updated SQl project

--- SQLProject/SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLProject/SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -34,36 +34,45 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
-SELECT * FROM `Facilities` WHERE membercost !=0
+SELECT name 
+FROM `Facilities` 
+WHERE membercost !=0
 
 /* Q2: How many facilities do not charge a fee to members? */
-SELECT 
-	COUNT(facid) 
-	FROM `Facilities` 
-	WHERE membercost !=0
+SELECT COUNT(facid) 
+FROM `Facilities` 
+WHERE membercost !=0
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-SELECT
-	facid, name, membercost,monthlymaintenance
-	FROM Facilities
-	WHERE membercost !=0 AND membercost < 0.2*monthlymaintenance
+SELECT facid, name, membercost,monthlymaintenance
+FROM Facilities
+WHERE membercost !=0 AND membercost < 0.2*monthlymaintenance
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
-
+SELECT *
+FROM Facilities
+WHERE facid in (1, 5) 
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance,
+CASE WHEN monthlymaintenance > 100 THEN "Expensive"
+ELSE "Cheap"
+END AS Label
+FROM Facilities
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
-
+SELECT firstname, surname
+FROM `Members`
+WHERE joindate=(SELECT MAX(joindate) FROM Members)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
